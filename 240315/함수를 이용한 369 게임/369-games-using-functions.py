5bfa45b008cd269dc72620b01af2290025077f74
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-03-15 21:26:38 +0900

    Title: 함수를 이용한 369 게임, Time: 252ms, Memory: 25MB, Status: Passed - CodeTree

--- 240315/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240315/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,6 +1,13 @@
 n,m = map(int,input().split())
 ans =0
+
 for k in range(n,m+1):
-    if k %3 ==0 or k%10 in [3,6,9] or k//10 in[3,6,9]:
+    if k %3 ==0:
         ans +=1
+        continue
+        
+    for i in range(len(str(k))):
+        if str(k)[i] in ["3","6","9"]:
+            ans+=1
+            break
 print(ans)
