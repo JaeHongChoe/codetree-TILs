4398b35b8c1d724dce3155d9591e61576421c4c1
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-03-15 21:03:19 +0900

    Title: 최소공배수 구하기, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240315/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240315/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,6 +1,6 @@
 n = list(map(int,input().split()))
 
-for k in range(max(n[0],n[1]), (n[0]*n[1])):
+for k in range(max(n[0],n[1]), (n[0]*n[1])+1):
     if k%n[0] ==0 and k%n[1] ==0:
         print(k)
         break
