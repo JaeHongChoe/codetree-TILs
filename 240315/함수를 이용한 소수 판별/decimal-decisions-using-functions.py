9fd7d0161719183e03d78a3a0f40d7cda1d3a749
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-03-15 21:34:02 +0900

    Title: 함수를 이용한 소수 판별, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240315/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,11 +1,14 @@
 n= list(map(int,input().split()))
-ans =0
 
-if len(n) <2:
+def test(n):
+    ans =0
+    if len(n) <2:
+        print(ans)
+        return
+
+    for k in range(n[0],n[1]+1):
+        if k %2 !=0 and k %3 !=0:
+            ans+=k
     print(ans)
-    exit(0)
 
-for k in range(n[0],n[1]+1):
-    if k %2 !=0 and k %3 !=0:
-        ans+=k
-print(ans)
+test(n)
