1f951ee3b33d4c072962ec2abc1f36a402391bf2
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-03-22 17:23:47 +0900

    Title: 행복한 수열의 개수, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,17 @@
+n,m = map(int,input().split())
+
+matrix = [list(map(int, input().split())) for _ in range(n)] 
+ans =0
+
+for k in range(n):
+    temp =[]
+    if len(set(matrix[k])) >= m:
+        ans +=1
+    for d in range(n):
+        if len(temp) >=m:
+            ans +=1 
+            break
+        if matrix[d][k] not in temp:
+            temp.append(matrix[d][k])
+
+print(ans)
