8313872300cb0992238c8b63e788c2e800b93252
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-04-30 20:45:26 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240430/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -5,10 +5,14 @@ grid =[
 ]
 min_range = 9999999
 for k in range(1,a-1):
-    temp = abs(grid[0][0] - grid[k][0]) + abs(grid[0][1] - grid[k][1])
-    temp2 = abs(grid[-1][0] - grid[k][0]) + abs(grid[-1][1] - grid[k][1])
-    total = temp+temp2
+    dist=0
+    prev_idx=0
+    for j in range(1, a):
+        if j == k:
+            continue
+        dist += abs(grid[prev_idx][0] - grid[j][0]) + abs(grid[prev_idx][1] - grid[j][1])
+        prev_idx = j
     
-    min_range = min(min_range, total)
+    min_range = min(min_range, dist)
 
 print(min_range)
