95b3c81f04d75febd43a2f1b88ca9d534212e1cf
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-04-30 15:38:32 +0900

    Title: Date to Date, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240430/Date to Date/date-to-date.py
+++ 240430/Date to Date/date-to-date.py
@@ -3,7 +3,7 @@ total=  a[3]- a[1] +1
 for k in range(a[0],a[2]):
     if k ==2: #28
         total += 28 
-    elif k % 2==0: #30
+    elif k in [2,4,6,9,11]: #30
         total += 30
     else: #31
         total += 31
