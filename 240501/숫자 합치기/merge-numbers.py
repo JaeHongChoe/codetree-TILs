cf69e7f1234babd807aaf40bde7d825245f171bf
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-05-01 02:17:42 +0900

    Title: 숫자 합치기, Time: 5054ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240501/숫자 합치기/merge-numbers.py
+++ 240501/숫자 합치기/merge-numbers.py
@@ -3,14 +3,19 @@ b = list(map(int,input().split()))
 
 b.sort()
 
-ans = sum(b)
+ans = 0
+check=0
 old =[]
+if len(b) %2 !=0:
+    check=1
+    ans+=b[-1]
 while(True):
     temp=[]
-    for k in range(0,a,2):
+    for k in range(0,a-check,2):
+        ans+=(b[k]+b[k+1])
         temp.append(b[k]+b[k+1])
     old = temp
     ans += sum(old)
-    if len(old) ==2:
+    if len(old) <=2:
         break
 print(ans)
