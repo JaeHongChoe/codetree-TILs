666619374e86cfa1d7026434f76c9c991b860172
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-04-30 23:53:02 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 358ms, Memory: 31MB, Status: Passed - CodeTree

--- 240430/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
+++ 240430/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -1,16 +1,17 @@
-a,bag = map(int,input().split())
+a,bag = map(float,input().split())
 money=[]
-for k in range(a):
+for k in range(int(a)):
     money.append(list(map(float,input().split())))
     money[k].append(money[k][1]/money[k][0])
-
 money.sort(key = lambda x : -x[2])
-ans=0
+ans=0.0
 
-for k in range(a):
+for k in range(int(a)):
     if bag - money[k][0] <0:
         ans += bag * money[k][2]
-        print(f"{ans:.3f}")
-        exit()
+        break
     bag -= money[k][0]
     ans += money[k][1]
+    
+    
+print(f"{ans:.3f}")
