636db9ab7ee3ee81af700db256f07e102e9a929e
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-04-30 15:28:23 +0900

    Title: Date to Date, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240430/Date to Date/date-to-date.py
+++ 240430/Date to Date/date-to-date.py
@@ -1,5 +1,5 @@
 a= list(map(int,input().split()))
-total=  a[3]- a[1] 
+total=  a[3]- a[1] +1
 for k in range(a[0],a[2]):
     if k ==2: #28
         total += 28 
@@ -8,6 +8,6 @@ for k in range(a[0],a[2]):
     else: #31
         total += 31
 
-if total ==0:
-    total+=1
+# if total ==0:
+#     total+=1
 print(total)
