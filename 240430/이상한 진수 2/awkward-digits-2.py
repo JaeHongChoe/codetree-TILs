dbbb22f39848f937cce0f0d934edbca8429e3699
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-04-30 19:51:38 +0900

    Title: 이상한 진수 2, Time: 102ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/이상한 진수 2/awkward-digits-2.py
+++ 240430/이상한 진수 2/awkward-digits-2.py
@@ -1,12 +1,14 @@
 a = input()
 a= list(a)
-# print(int(a,2))
+check=0
 
 for k in range(1,len(a)):
     if a[k] == '0':
         a[k] = '1'
+        check=1
         break
-    
+if check ==0:
+    a[-1] ='0'
 
 ans = "".join(a)
 
