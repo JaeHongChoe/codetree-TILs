72ee5f551e2a3937b3d8107667f55b6dd8cb1e1a
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-05-01 16:52:52 +0900

    Title: 가장 많은 데이터, Time: 352ms, Memory: 37MB, Status: Passed - CodeTree

--- 240501/가장 많은 데이터/most-frequent-data.py
+++ 240501/가장 많은 데이터/most-frequent-data.py
@@ -7,13 +7,12 @@ n = [
 dic ={}
 ans =0
 for k in n:
-    
     if k not in dic:
         dic[k] =1
     else:
         dic[k] += 1
-        if ans < dic[k]:
-            ans=dic[k]
+    if ans < dic[k]:
+        ans=dic[k]
     
 
 print(ans)
