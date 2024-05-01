fcc5bc98e4f9eb18ee42cc2c0ddaeec7337a4c19
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-05-01 18:47:39 +0900

    Title: 정수 찾기, Time: 240ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 240501/정수 찾기/find-an-integer.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = list(map(int,input().split()))
+b = set(b)
+n = int(input())
+temp = list(map(int,input().split()))
+
+for k in temp:
+    if k in b:
+        print(1)
+    else:
+        print(0)
