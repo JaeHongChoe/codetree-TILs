3f2a3798f41e77b087c777b1ea70adabf40eef9b
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-05-01 15:24:42 +0900

    Title: 회의실 준비 구현, Time: 252ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240501/회의실 준비 구현/implement-scheduling-meeting-room.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = [ 
+    list(map(int,input().split()))
+    for _ in range (a)
+]
+
+b.sort(key = lambda x:x[1])
+cnt=1
+check = b[0][1]
+for k in range(1,a):
+    if check <= b[k][0]:
+        cnt+=1
+        check = b[k-1][1]
+
+print(cnt)
