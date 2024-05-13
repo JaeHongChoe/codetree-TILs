6a30995283efe4577bdb938a709d85eb4265822d
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-05-13 18:43:57 +0900

    Title: 마을 구분하기, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240513/마을 구분하기/seperate-village.py
@@ -0,0 +1,49 @@
+a = int(input())
+
+graph = [
+    list(map(int,input().split()))
+    for _ in range(a)
+]
+
+visited = [
+    [0 for _ in range(a)]
+    for _ in range(a)
+]
+
+ans = []
+
+def in_range(x, y):
+    return 0 <= x and x < a and 0 <= y and y < a
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] or graph[x][y] ==0:
+        return False
+    return True
+
+
+def dfs(x,y):
+    global cnt
+    # 0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
+    dxs, dys = [1,0,-1,0],[0,-1,0,1]
+
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        if can_go(new_x, new_y):
+            visited[new_x][new_y] =1
+            cnt +=1
+            dfs(new_x,new_y)
+            
+
+for i in range(a):
+    for k in range(a):
+        cnt =0
+        dfs(i,k)
+        if cnt != 0:
+            ans.append(cnt)
+        
+
+print(len(ans))
+for k in sorted(ans):
+    print(k)
