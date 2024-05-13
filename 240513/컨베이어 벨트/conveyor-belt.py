7e4c8d101d096123e0d67c4056a200a156239407
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-05-13 19:27:53 +0900

    Title: 컨베이어 벨트, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240513/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,20 @@
+a,b = map(int,input().split())
+
+grid = [
+    list(map(int,input().split()))
+    for _ in range(2)
+]
+ans =[]
+grid = sum(grid,[])
+
+if a*2 < b:
+    b = a*2 % b 
+
+new_b = a*2 - b
+ans.extend(grid[new_b:])
+ans.extend(grid[:new_b])
+
+for k in range(1,len(ans)+1):
+    print(ans[k-1], end = " ")
+    if k % a ==0 :
+        print()
