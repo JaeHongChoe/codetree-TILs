40c926b07d67ec2b57e87b1d8e5d2831382a8c59
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-05-13 18:12:06 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240513/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,44 @@
+a,b = map(int,input().split())
+
+graph = [
+    list(map(int,input().split()))
+    for _ in range(b)
+]
+
+visited = [
+    [0 for _ in range(a)]
+    for _ in range(b)
+]
+
+ans = [
+    [0 for _ in range(a)]
+    for _ in range(b)
+]
+
+def in_range(x,y):
+    return 0 <= x and x <a and 0 <= y and y < b
+
+def can_go(x, y):
+    if not in_range(x,y):
+        return False
+    if visited[x][y] or graph[x][y] ==0:
+        return False
+    return True
+
+def dfs(x, y):
+    global order
+
+    dxs, dys = [1,0],[0,1]
+
+    for dx,dy in zip(dxs, dys):
+        new_x, new_y = dx + x, dy + y
+        if can_go(new_x, new_y):
+            visited[new_x][new_y] = 1
+            ans[new_x][new_y] = 1
+            dfs(new_x, new_y)
+
+
+ans[0][0] = 1
+visited[0][0]=1
+dfs(0,0)
+print(ans[-1][-1])
