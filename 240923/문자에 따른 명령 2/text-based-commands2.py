9fc23835903461ca04f8ee4302bfb88ae35461af
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-09-23 21:26:39 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240923/문자에 따른 명령 2/text-based-commands2.py
+++ 240923/문자에 따른 명령 2/text-based-commands2.py
@@ -4,6 +4,7 @@ a = input()
 d = [(0,-1),(-1,0),(0,1),(1,0)]
 x,y=0,0
 d_idx=3
+check=0
 
 for i in range(len(a)):
     if a[i] =="L":
@@ -12,6 +13,11 @@ for i in range(len(a)):
         d_idx -= 1
     else:
         d_idx = d_idx%4
-x= x+d[d_idx][1]
-y= y+d[d_idx][0]
-print(x,y)
+        check =1
+
+if not check:
+    print(0,0)
+else:
+    x= x+d[d_idx][1]
+    y= y+d[d_idx][0]
+    print(x,y)
