988ef48a8bd4845e458aa2ac04fc684a94700873
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-09-23 21:04:26 +0900

    Title: 방향에 맞춰 이동, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240923/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,26 @@
+a = int(input())
+grid = [
+    list(map(str,input().split()))
+    for _ in range(a)
+]
+x,y =0,0
+#서남동북
+b = [(0,-1),(-1,0),(0,1),(1,0)]
+for i in range(a):
+    if grid[i][0] == "N":
+        for _ in range(int(grid[i][1])):
+            x+=b[3][1]
+            y+=b[3][0]
+    elif grid[i][0] =="E":
+        for _ in range(int(grid[i][1])):
+            x+=b[2][1]
+            y+=b[2][0]
+    elif grid[i][0] =="S":
+        for _ in range(int(grid[i][1])):
+            x+=b[1][1]
+            y+=b[1][0]
+    elif grid[i][0] =="W":
+        for _ in range(int(grid[i][1])):
+            x+=b[1][1]
+            y+=b[1][0]
+print(x,y)
