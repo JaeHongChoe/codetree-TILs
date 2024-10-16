447d680940f0552281813fef0838bd260104410c
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-10-16 18:15:56 +0900

    Title: 앞에서부터 삭제하기 2, Time: 162ms, Memory: 32MB, Status: Passed - CodeTree

--- 241016/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 241016/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,21 +1,32 @@
 import heapq
-count = int(input())
-arr_list = list(map(int,input().split()))
 
-pq_max = []
-pq_min = []
+# 입력:
+n = int(input())
+arr = list(map(int, input().split()))
 
-for i in range(count):
-    heapq.heappush(pq_max, -arr_list[i])
-    heapq.heappush(pq_min, arr_list[i])
+# 변수 선언
+sum_val = 0
+max_avg = 0
+pq = []
 
+heapq.heappush(pq, arr[n - 1])
+sum_val += arr[n - 1]
+# k가 N - 2일 때부터 1일 때까지 거꾸로 탐색합니다.
+# priority queue를 이용하여 진행합니다.
+for i in range(n - 2, 0, -1):
+    # 앞에서부터 K개를 삭제하고 나면
+    # 뒤에 i ~ n - 1 까지의 숫자만이 남습니다.
+    heapq.heappush(pq, arr[i])
+    sum_val += arr[i]
 
-for i in range(count):
-    if arr_list[i] == -heapq.heappop(pq_max):
-        temp = sum(arr_list[i-1:])
-        length = len(arr_list[i-1:])
-        if length > 2:
-            temp -= min(arr_list[i-1:])
-            length -= 1
-        print(f"{temp/length:.2f}")
-        break
+    # 남아있는 정수 중 가장 작은 숫자를 찾아
+    # 그 숫자를 제외한 평균을 구합니다.
+    min_num = pq[0]
+    avg = (sum_val - min_num) / (n - i - 1)
+
+    # 평균이 최대가 된다면 정답을 현재 평균으로 갱신해줍니다.
+    if max_avg < avg:
+        max_avg = avg
+
+# 평균값의 최대를 출력합니다.
+print(f"{max_avg:.2f}")
