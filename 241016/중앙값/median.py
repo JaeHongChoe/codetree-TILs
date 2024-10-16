02456be02cceb58273e3f04374daca227cdc3f90
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-10-16 18:43:17 +0900

    Title: 중앙값, Time: 5098ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 241016/중앙값/median.py
+++ 241016/중앙값/median.py
@@ -3,9 +3,8 @@ count = int(input())
 for _ in range(count):
     length = int(input())
     lists = list(map(int,input().split()))
-    # arr = []
-    # arr.append(lists[0])
+    print(lists[0], end = " ")
     for i in range((length//2)+1):
-        # arr.append(lists[(i*2)//2])
-        print(lists[(i*2)//2], end = " ")
+        if i != 0:
+            print(sorted(lists[0:(i*2)+1])[(i*2)//2], end = " ")
     print()
