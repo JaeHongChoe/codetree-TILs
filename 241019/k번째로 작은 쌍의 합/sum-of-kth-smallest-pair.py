a300d40941890c664e53682d4020a3dc1eed8e81
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-10-19 20:08:12 +0900

    Title: k번째로 작은 쌍의 합, Time: 619ms, Memory: 39MB, Status: Passed - CodeTree

--- 241019/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 241019/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -3,27 +3,18 @@ n, m, k = list(map(int,input().split()))
 pq =[]
 n_arr = list(map(int,input().split()))
 m_arr = list(map(int,input().split()))
+n_arr = sorted(n_arr)
+m_arr = sorted(m_arr)
 
-n_visit = [0] * len(n_arr)
-m_visit = [0] * len(m_arr)
 
-def permutations(n, new_arr):
-    global arr
-    # 순서 상관 0, 중복 X
-    if len(new_arr) == n:
-        heapq.heappush(pq,sum(new_arr))
-        return
-    for i in range(len(n_arr)):
-        if not n_visit[i]:
-            n_visit[i] = 1
-            for k in range(len(m_visit)):
-                if not m_visit[k]:
-                    m_visit[k] = 1
-                    permutations(n, [n_arr[i]] + [m_arr[k]])
-                    m_visit[k] = 0
-            n_visit[i] = 0
+for i in range(n):
+    heapq.heappush(pq, (n_arr[i] + m_arr[0], i, 0))
 
-permutations(2, [])
-for _ in range(k):
-    ans = heapq.heappop(pq)
-print(ans)
+for i in range(k-1):
+    _, idx1, idx2 = heapq.heappop(pq)
+    idx2 += 1
+    if idx2 < m:
+        heapq.heappush(pq, (n_arr[idx1]+m_arr[idx2],idx1,idx2))
+
+sum_,_,_ = heapq.heappop(pq)
+print(sum_)
