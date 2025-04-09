3d1ef3607ec95dca3ca3fc8cc0c4e5c4110fa25b
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-04-09 19:48:10 +0900

    Title: 신기한 타일 뒤집기, Time: 71ms, Memory: 17MB, Status: Passed - Codetree

--- 250409/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 250409/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,11 +1,15 @@
 n = int(input())
 commands = [tuple(input().split()) for _ in range(n)]
 line = {}
+status = 0
 for num, direction in commands:
-    for i in range(int(num)):
-        if direction == 'R':
-            line[i] = "R"
-        else:
-            line[i] = "L"
+    if direction == 'R':
+        for i in range(int(num)):
+            line[status+i] = "R"
+        status += int(num) -1
+    else:
+        for i in range(int(num)):
+            line[status-i] = "L"
+        status -= int(num) -1
 print(list(line.values()).count("L"),list(line.values()).count("R"))
 # Please write your code here.
