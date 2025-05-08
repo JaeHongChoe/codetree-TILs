16b8421535e744a794d6907fb248c9db59b91c74
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-08 16:07:41 +0900

    Title: 일렬로 서있는 소 2, Time: 73ms, Memory: 17MB, Status: Passed - Codetree

--- 250508/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 250508/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,7 +5,7 @@ ans = 0
 for i in range(N):
     for k in range(i+1,N):
         for j in range(k+1,N):
-            if A[i] < A[k] and A[k] < A[j]:
+            if A[i] <= A[k] and A[k] <= A[j]:
                 ans+=1
 print(ans)
 # Please write your code here.
