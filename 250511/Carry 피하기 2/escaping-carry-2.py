b27cccf308d36aaf37135e9a5ff019ab5167fc55
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-11 00:37:30 +0900

    Title: Carry 피하기 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250511/Carry 피하기 2/escaping-carry-2.py
+++ 250511/Carry 피하기 2/escaping-carry-2.py
@@ -2,6 +2,7 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 ans = -1
 
+
 for i in range(n-2):
     for k in range(i+1,n-1):
         for j in range(k+1,n):
@@ -9,14 +10,14 @@ for i in range(n-2):
             temp1 = arr[i]
             temp2 = arr[k]
             temp3 = arr[j]
-            for m in range(1,total+1):
+            for m in range(total):
                 flag = 1
-                if 10 <= (temp1 % 10 + temp2 % 10 + temp3 % 10):
+                if 9 < (temp1 % 10 + temp2 % 10 + temp3 % 10):
                     flag = -1
                     break
-                temp1 = temp1// 10**m
-                temp2 = temp2// 10**m
-                temp3 = temp3// 10**m
+                temp1 = temp1// 10
+                temp2 = temp2// 10
+                temp3 = temp3// 10
 
             if ans < (arr[i] + arr[k] + arr[j]) * flag:
                 ans = arr[i] + arr[k] + arr[j]
