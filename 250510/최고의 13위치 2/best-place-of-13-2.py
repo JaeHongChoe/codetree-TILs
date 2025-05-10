c3b78ca4cb847bf595884a0fbc569779d4d27c20
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-10 19:15:12 +0900

    Title: 최고의 13위치 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250510/최고의 13위치 2/best-place-of-13-2.py
+++ 250510/최고의 13위치 2/best-place-of-13-2.py
@@ -4,8 +4,11 @@ ans = 0
 for i in range(n):
     for k in range(n-2):
         temp1 = arr[i][k] + arr[i][k+1] + arr[i][k+2]
-        for j in range(i+1,n):
-            for m in range(n-2):
+        for j in range(i,n):
+            info = 0
+            if i == j:
+                info = 3
+            for m in range(n-2-info):
                 temp2 = arr[j][m] + arr[j][m+1] + arr[j][m+2]
                 if ans < temp1 + temp2:
                     ans = temp1 + temp2
