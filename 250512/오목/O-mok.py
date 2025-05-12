5bd72a6e779bf3d604c05475bc013c0fe53d128f
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-12 15:46:36 +0900

    Title: 오목, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250512/오목/O-mok.py
+++ 250512/오목/O-mok.py
@@ -1,5 +1,5 @@
 board = [list(map(int, input().split())) for _ in range(19)]
-
+ans = 0
 def in_range(x, y):
     return 0 <= x and x < 19 and 0 <= y and y < 19
     
@@ -12,20 +12,23 @@ def in_range(x, y):
 def check(x, y,dirr,flag):
     dxs, dys = [1, 0, 1, 1], [0, 1, -1, 1]
     nx, ny = x + dxs[dirr], y + dys[dirr]
-    if in_range(nx,ny) and board[nx][ny] == board[x][y]:
-        flag +=1
-        if flag >= 5:
-            print(board[x][y])
-            print(1 + nx - dxs[dirr]*2 ,1 + ny - dys[dirr]*2)
-            return ""
-        else:
-            check(nx,ny,dirr,flag)
+    if flag >= 5:
+        print(board[x][y])
+        print(1 + nx - dxs[dirr]*3 ,1 + ny - dys[dirr]*3)
+        return 1
+    else:
+        if in_range(nx,ny) and board[nx][ny] == board[x][y]:
+            flag +=1
+            return check(nx,ny,dirr,flag)
 
 for i in range(19):
     for k in range(19):
-        if board[i][k] != 0:
+        if board[i][k] != 0 and ans == 0:
             for j in range(4):
                 flag = 1
-                check(i,k,j,flag)
+                if check(i,k,j,flag):
+                    ans = 1
+if ans == 0:
+    print(0)
 
 # Please write your code here.
