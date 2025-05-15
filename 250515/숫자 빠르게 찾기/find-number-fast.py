8edb89268373112ee5c03ce76c730fef77e005f6
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-15 22:11:04 +0900

    Title: 숫자 빠르게 찾기, Time: 170ms, Memory: 23MB, Status: Passed - Codetree

--- 250515/숫자 빠르게 찾기/find-number-fast.py
+++ 250515/숫자 빠르게 찾기/find-number-fast.py
@@ -2,14 +2,18 @@ n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 queries = [int(input()) for _ in range(m)]
 
-def check():
-    for k in range(n):
-        if queries[i] == arr[k]:
-            return k+1
-    return -1
-
 for i in range(m):
-    print(check())
-
-
+    left, right = 0 , n -1
+    while True:
+        mid = (left + right) //2
+        if arr[mid] == queries[i]:
+            print(mid+1)
+            break
+        elif left >= right:
+            print(-1)
+            break
+        if arr[mid] > queries[i]:
+            right = mid -1
+        else:
+            left = mid + 1
 # Please write your code here.
