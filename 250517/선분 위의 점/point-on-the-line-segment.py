702b0d3de41cea8af760d83cc508c448c8d90d39
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-17 23:30:38 +0900

    Title: 선분 위의 점, Time: 1057ms, Memory: 28MB, Status: Time Limit Exceed - Codetree

---
+++ 250517/선분 위의 점/point-on-the-line-segment.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+points = list(map(int, input().split()))
+segments = [tuple(map(int, input().split())) for _ in range(m)]
+
+for seg_left,seg_right in segments:
+    ans = 0
+    for k in points:
+        if k >= seg_left and k <= seg_right:
+            left, right = seg_left, seg_right
+            while left <= right:
+                mid = (left + right) // 2
+                if mid < k:
+                    left = mid +1
+                elif mid > k:
+                    right = mid - 1
+                else:
+                    ans += 1
+                    break
+    print(ans)
+
+# Please write your code here.
