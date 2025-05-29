f2a231b170b8f36385bfbf18a80f664724f3cbcd
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-29 13:11:17 +0900

    Title: 자연수 n개의 합, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250529/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -0,0 +1,13 @@
+s = int(input())
+ans = 0
+left , right = 1, s//2
+while left <= right:
+    mid = (left+right)//2
+    if mid *(mid+1)//2 <=s:
+        ans = max(ans,left)
+        left = mid +1
+    else:
+        right = mid -1
+print(ans)
+
+# Please write your code here.
