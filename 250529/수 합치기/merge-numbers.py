3ec686d95b597c397421b3c5e9bf53f25f5a184b
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-29 17:06:34 +0900

    Title: 수 합치기, Time: 201ms, Memory: 23MB, Status: Passed - Codetree

--- 250529/수 합치기/merge-numbers.py
+++ 250529/수 합치기/merge-numbers.py
@@ -1,15 +1,29 @@
+import heapq
+
+# 변수 선언 및 입력:
 n = int(input())
 arr = list(map(int, input().split()))
-tmp = arr[0]
+
+pq = []
 ans = 0
 
-for i in range(1,n-1):
-    if tmp < arr[i+1]:
-        tmp += arr[i]
-        ans += tmp
-    else:
-        arr[1+i] = arr[1+i] + arr[i]
-        ans += arr[1+i]
 
-print(ans+tmp+arr[-1])
-# Please write your code here.
+# 우선순위 큐에 원소들을 전부 넣어줍니다.
+# 작은 숫자 2개를 골라 합치는 것이 항상 유리함을 이용해야 하므로
+# 작은 숫자가 먼저 골라질 수 있도록 해야합니다.
+for elem in arr:
+    heapq.heappush(pq, elem)
+
+# 원소가 2개 이상이면 계속
+# 가장 작은 숫자 2개를 골라
+# 합치는 것을 반복합니다.
+while len(pq) > 1:
+    x1 = heapq.heappop(pq)
+    x2 = heapq.heappop(pq)
+
+    # 가장 작은 숫자 2개를 더하기 위한 비용을 답에 더해주고,
+    # 두 숫자를 합친 결과를 우선순위 큐에 다시 넣어줍니다.
+    ans += (x1 + x2)
+    heapq.heappush(pq, x1 + x2)
+
+print(ans)
