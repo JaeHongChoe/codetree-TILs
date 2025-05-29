9f7d4719095ded3b67a1ffa0f6dffffbcd662eab
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-05-29 23:44:11 +0900

    Title: 외판원 순회, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250529/외판원 순회/traveling-salesman-problem.py
+++ 250529/외판원 순회/traveling-salesman-problem.py
@@ -3,13 +3,18 @@ A = [list(map(int, input().split())) for _ in range(n)]
 visited = [0]*n
 arr = [i for i in range(n)]
 ans = 99999999
+
+def check(check_arr):
+    if abs(check_arr[0]-check_arr[-1]) != 1 or abs(check_arr[0] - check_arr[-1]) == n-1:
+        return False
+    return True
+
 def chosoe(num, new_arr):
     global arr, ans, A
-    if len(new_arr) == num:
+    if len(new_arr) == num and check(new_arr):
         temp = 0
         for k in range(num):
-            if A[k][new_arr[k]] ==0:
-                temp = 99999999
+            if A[k][new_arr[k]] == 0:
                 return
             temp += A[k][new_arr[k]]
         ans = min(ans, temp)
