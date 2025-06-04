d8867f199dc443e749a794ea25a778019520597d
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-06-05 01:44:27 +0900

    Title: 돌 잘 치우기, Time: 1072ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

--- 250605/돌 잘 치우기/clear-stones-well.py
+++ 250605/돌 잘 치우기/clear-stones-well.py
@@ -1,7 +1,6 @@
 n, k, m = map(int, input().split())
 
 grid = [list(map(int, input().split())) for _ in range(n)]
-ans_visited = [[0]*n for _ in range(n)]
 arr = []
 arr_back=[]
 for i in range(n):
@@ -29,27 +28,27 @@ def in_range(y,x):
     return x >=0 and x <= n-1 and y >= 0 and y <= n-1
 
 def bfs(loc):
-    global ans_visited, ans
+    global ans_visited
     d = [(0,-1),(1,0),(0,1),(-1,0)]
-    for i in range(len(arr_back)):
-        q.append((r[loc],c[loc]))
+    ans = 0
+    ans_visited = [[0]*n for _ in range(n)]
+    for i in range(k):
+        q.append((r[i],c[i]))
         visited = [[0]*n for _ in range(n)]
-        visited[r[loc]][c[loc]] = 1
+        visited[r[i]][c[i]] = 1
         while q:
             y,x = q.popleft()
-            if ans_visited[y][x] == 0 and not [y,x] in arr_back[i]:
+            if ans_visited[y][x] == 0 and not [y,x] in arr_back[loc]:
                 ans_visited[y][x] = 1
                 ans +=1
             for j in range(len(d)):
                 dy, dx = d[j]
                 oy, ox = dy + y, dx+x
-                if in_range(oy,ox) and (grid[oy][ox] != 1 or [oy,ox] in arr_back[i]) and visited[oy][ox] ==0:
+                if in_range(oy,ox) and (grid[oy][ox] != 1 or [oy,ox] in arr_back[loc]) and visited[oy][ox] ==0:
                     visited[oy][ox] = 1
                     q.append((oy,ox))
+    return ans+m
             
-
-
-
 
 
 from collections import deque
@@ -57,8 +56,8 @@ q = deque()
 # oy,ox = 1,3
 # print((grid[2][0] != 1 or [oy,ox] in [[1,2],[2,0]]))
 ans = 0
-for i in range(k):
-    bfs(i)
-    # ans = max(ans,total)
-print(ans+m)
+for i in range(len(arr_back)):
+    total = bfs(i)
+    ans = max(ans,total)
+print(ans)
 # Please write your code here.
