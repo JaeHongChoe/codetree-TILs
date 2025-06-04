eb0dd61489752a7c679a9deec765b2843b9656e8
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-06-04 21:26:55 +0900

    Title: K번 최댓값으로 이동하기, Time: 75ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250604/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 250604/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -8,39 +8,48 @@ def in_range(y,x):
     return x >=0 and x <= n-1 and y >= 0 and y <= n-1
 
 def bfs():
+    global visited
     d= [(0,-1),(1,0),(0,1),(-1,0)]
     ans = (r,c)
     for _ in range(k):
         # if not q:
         #     return ans
-        l,y,x = q.popleft()
         max_loc = (-1,1,1)
         # visited = [[0]*n for _ in range(n)]
         # visited[r-1][c-1] = 1
-        for i in range(len(d)):
-            dy, dx = d[i][0]+y, d[i][1]+x
-            if in_range(dy,dx) and visited[dy][dx] == 0 and grid[dy][dx] < target:
-                temp_loc = (grid[dy][dx],dy*-1,dx*-1)
-                if max_loc < temp_loc:
-                    max_loc = temp_loc
+        while q:
+            # max_loc = (-1,1,1)
+            l,y,x = q.popleft()
+            for i in range(len(d)):
+                dy, dx = d[i][0]+y, d[i][1]+x
+                if in_range(dy,dx) and visited[dy][dx] == 0 and grid[dy][dx] < target:
+                    visited[dy][dx] = 1
+                    q.append((grid[dy][dx],dy,dx))
+                    temp_loc = (grid[dy][dx],dy*-1,dx*-1)
+                    if max_loc < temp_loc:
+                        max_loc = temp_loc
+        # print(max_loc)
         if max_loc == (-1,1,1):
             return ans 
         else:
-            y,x = find_loc(max_loc[0])
-            q.append((max_loc[0],y,x))
-            ans = (y+1,x+1)
+            my,mx,visited = find_loc(max_loc[0])
+            q.append((max_loc[0],my,mx))
+            ans = (my+1,mx+1)
+        # print(visited)
+
     return ans
 def find_loc(loc):
     best = 0
+    visited = [[0]*n for _ in range(n)]
     for i in range(n):
         for k in range(n):
-            if loc == grid[i][k]:
+            if loc <= grid[i][k]:
                 visited[i][k] = 1
             if loc == grid[i][k] and best==0:
                 best=1
                 best_y, best_x = i,k
                 # print(i,k)
-    return best_y, best_x
+    return best_y, best_x, visited
 
 q= deque()
 q.append((grid[r-1][c-1],(r-1),(c-1)))
